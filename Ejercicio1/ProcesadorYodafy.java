6e72b62b588742f4803fd699d0a67b336a05b275
scmarquez <scmarquez@correo.ugr.es>
2016-11-28 19:57:22 +0100

    ejercicios

--- Ejercicio1/ProcesadorYodafy.java
+++ Ejercicio1/ProcesadorYodafy.java
@@ -51,7 +51,7 @@ public class ProcesadorYodafy {
 			////////////////////////////////////////////////////////
 			// read ... datosRecibidos.. (Completar)
 			////////////////////////////////////////////////////////
-			
+			bytesRecibidos = inputStream.read(datosRecibidos);
 			// Yoda hace su magia:
 			// Creamos un String a partir de un array de bytes de tamaño "bytesRecibidos":
 			String peticion=new String(datosRecibidos,0,bytesRecibidos);
@@ -64,7 +64,7 @@ public class ProcesadorYodafy {
 			////////////////////////////////////////////////////////
 			// ... write ... datosEnviar... datosEnviar.length ... (Completar)
 			////////////////////////////////////////////////////////
-			
+			outputStream.write(datosEnviar,0,datosEnviar.length);
 			
 			
 		} catch (IOException e) {
